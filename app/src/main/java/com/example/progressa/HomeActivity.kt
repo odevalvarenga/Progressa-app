1c8f0bc9ec5ccf685216e1b6a9788d6f3dfccac2
Sammysantarosa <158489125+Sammysantarosa@users.noreply.github.com>
2026-05-02 21:47:39 -0300

    feat: tela Home

--- app/src/main/java/com/example/progressa/HomeActivity.kt
+++ app/src/main/java/com/example/progressa/HomeActivity.kt
@@ -3,10 +3,10 @@ package com.example.progressa
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
-
+class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        // Certifique-se que o arquivo activity_home.xml existe em res/layout
+        setContentView(R.layout.activity_home)
     }
 }
